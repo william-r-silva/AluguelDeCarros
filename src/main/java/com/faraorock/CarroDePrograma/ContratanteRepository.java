4566fdba39492c98798e6193e509e24602ceab6b
FaraoAten <atenfernandes@gmail.com>
2021-02-04 21:34:07 -0300

    queryMethods

--- src/main/java/com/faraorock/CarroDePrograma/ContratanteRepository.java
+++ src/main/java/com/faraorock/CarroDePrograma/ContratanteRepository.java
@@ -1,7 +1,10 @@
 package com.faraorock.CarroDePrograma;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContratanteRepository extends JpaRepository<Contratante, Integer> {
-
+	List<Contratante> findBynomeContainingOrderBynome(String nome);
+	List<Contratante> findfindBycnhContaining(int cnh);
 }
--- src/main/java/com/faraorock/CarroDePrograma/FilialRepository.java
+++ src/main/java/com/faraorock/CarroDePrograma/FilialRepository.java
@@ -1,7 +1,11 @@
 package com.faraorock.CarroDePrograma;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FilialRepository extends JpaRepository<Filial, Integer> {
-
+	List<Filial> findBycepContaining(int cep);
+	List<Filial> findBycidadeContaining(String cidade);
+	List<Filial> findByestadoContaining(String estado);
 }
