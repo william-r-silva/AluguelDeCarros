5a244afb47419f7c820387c502f96d4aac6aa4aa
RockpunkZikaGG <william.rocha.silva.wrs@gmail.com>
2021-02-28 17:11:55 -0300

    Finalizado Frame

--- src/main/java/com/faraorock/CarroDePrograma/CarroDeProgramaApplication.java
+++ src/main/java/com/faraorock/CarroDePrograma/CarroDeProgramaApplication.java
@@ -4,10 +4,10 @@ import com.faraorock.CarroDePrograma.Persistencia.FilialRepository;
 import com.faraorock.CarroDePrograma.Persistencia.MecanicaParceiraRepository;
 import com.faraorock.CarroDePrograma.Persistencia.CarroRepository;
 import com.faraorock.CarroDePrograma.Persistencia.ContratanteRepository;
+import com.faraorock.CarroDePrograma.View.FilialViewFrame;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import com.faraorock.CarroDePrograma.View.MainView;
 
 @SpringBootApplication
 public class CarroDeProgramaApplication {
@@ -26,8 +26,33 @@ public class CarroDeProgramaApplication {
 	
 	@Bean
 	public FilialRepository run2(FilialRepository fr) {
-		MainView.criaUI();
-		return fr;
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (ClassNotFoundException ex) {
+                java.util.logging.Logger.getLogger(FilialViewFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(FilialViewFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(FilialViewFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(FilialViewFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
+            //</editor-fold>
+
+            /* Create and display the form */
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                     new FilialViewFrame().setVisible(true);
+                }
+            });
+            
+            return fr;
 	}
 
 	@Bean
